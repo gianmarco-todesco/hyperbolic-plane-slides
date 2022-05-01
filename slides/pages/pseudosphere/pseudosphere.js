a12a1a75e74fcc0b7e73c33abfa49d3b2b8d8862
gianmarco-todesco <gianmarco.todesco@gmail.com>
2022-05-01 15:25:41 +0200

    .

--- slides/pages/pseudosphere/pseudosphere.js
+++ slides/pages/pseudosphere/pseudosphere.js
@@ -1,6 +1,9 @@
 'use strict';
 
 let canvas, engine, scene, camera;
+let param1 = 0;
+let param1Speed = 0;
+
 const slide = {
     name: 'Pseudosphere'
 };
@@ -39,6 +42,29 @@ function setup() {
 
     // resize event
     window.addEventListener("resize", onResize);
+
+    scene.onKeyboardObservable.add((kbInfo) => {
+        switch (kbInfo.type) {
+          case BABYLON.KeyboardEventTypes.KEYDOWN:
+            onKeyDown(kbInfo.event); // key, code
+            break;
+          case BABYLON.KeyboardEventTypes.KEYUP:
+            onKeyUp(kbInfo.event); 
+            break;
+        }
+    });
+
+    scene.registerBeforeRender(() => {
+        let dt = engine.getDeltaTime()*0.001;
+        if(param1Speed>0) {
+            param1 += param1Speed*dt;
+            if(param1>1) {param1 = 1; param1Speed=0;}
+        } else if(param1Speed<0) {
+            param1 += param1Speed*dt;
+            if(param1<0) {param1 = 0; param1Speed=0;}
+
+        }
+    });
 }
 
 function cleanup() {
@@ -57,18 +83,239 @@ function onResize() {
     engine.resize();
 }
 
-function populateScene() {
-    let torus = BABYLON.MeshBuilder.CreateTorus('torus',{
-        diameter:6,
-        thickness:1,
-        tessellation:70
 
-    },scene);
-    torus.material = new BABYLON.StandardMaterial('mat',scene);
-    torus.material.diffuseColor.set(0.8,0.4,0.1);
+function onKeyDown(e) {
+    if(e.key == 'q') param1Speed = 1;
+}
 
+function onKeyUp(e) {
+    if(e.key == 'q') param1Speed = -1;
+}
+
+
+class Surface {
+    constructor(f, nu, nv, scene) {
+        this.nu = nu;
+        this.nv = nv;
+        let vd = new BABYLON.VertexData();
+        vd.positions = [];
+        vd.normals = [];
+        vd.indices = [];
+        vd.uvs = [];
+        for(let i=0;i<nu;i++) {
+            let u = i/(nu-1);
+            for(let j=0;j<nv;j++) {
+                let v = j/(nv-1);
+                let p = f(u,v);
+                let nrm = this.computeNormal(f,u,v);
+                vd.positions.push(p.x,p.y,p.z);
+                vd.normals.push(nrm.x,nrm.y,nrm.z);
+                vd.uvs.push(u,v);
+            }
+        }
+        for(let i=0;i+1<nu;i++) {
+            for(let j=0;j+1<nv;j++) {
+                let k = i*nv+j;
+                vd.indices.push(k,k+1,k+1+nv, k,k+1+nv,k+nv);
+            }
+        }
+        /*
+            vd.normals = [];
+            BABYLON.VertexData.ComputeNormals(
+                vd.positions, 
+                vd.indices, 
+                vd.normals);
+        */
+        let mesh = this.mesh = new BABYLON.Mesh('surface', scene);
+        vd.applyToMesh(mesh, true);
+    }
+
+    computeNormal(f,u,v) {
+        const h = 0.0001;
+        let dfdu=f(u+h,v).subtract(f(u-h,v));
+        let dfdv=f(u,v+h).subtract(f(u,v-h));
+        return BABYLON.Vector3.Cross(dfdu,dfdv).normalize();
+    }
+
+    update(f) {
+        let positions = this.mesh.getVerticesData(BABYLON.VertexBuffer.PositionKind);
+        let normals = this.mesh.getVerticesData(BABYLON.VertexBuffer.NormalKind);
+        const nu = this.nu;
+        const nv = this.nv;
+        let k = 0;
+        for(let i=0;i<nu;i++) {
+            let u = i/(nu-1);
+            for(let j=0;j<nv;j++) {
+                let v = j/(nv-1);
+                let p = f(u,v);
+                let nrm = this.computeNormal(f,u,v);
+                positions[k]=p.x; 
+                positions[k+1]=p.y; 
+                positions[k+2]=p.z; 
+                normals[k]=nrm.x; 
+                normals[k+1]=nrm.y; 
+                normals[k+2]=nrm.z; 
+                k += 3;
+            }
+        }
+        this.mesh.updateVerticesData(BABYLON.VertexBuffer.PositionKind, positions);
+        this.mesh.updateVerticesData(BABYLON.VertexBuffer.NormalKind, normals);
+        
+    }
+}
+
+
+function makeCheckboardTexture(scene) {
+    const w = 1024, h = 1024;
+    let tx = new BABYLON.DynamicTexture('a', { width:w, height:h}, scene);
+    let ctx = tx.getContext();
+    ctx.fillStyle = 'magenta'; // 'transparent';
+    ctx.fillRect(0,0,w,h);
+    let dx = 128, dy = 64;
+    ctx.fillStyle = 'cyan';
+    for(let i=0; i*dy<h; i++) {
+        for(let j=0;j*dx<w;j++) {
+            if((i+j)&1) {
+                ctx.fillRect(j*dx,i*dy,dx,dy);
+            }
+        }
+    }
+    ctx.fillStyle = 'black';
+    for(let i=0; i*dy<h; i++) ctx.fillRect(0,i*dy-3,w,7);
+    for(let j=0; j*dx<w; j++) ctx.fillRect(j*dx-3,0,7,h);
+
+
+    tx.update();
+    tx.hasAlpha = true;
+    return tx;
+}
+
+
+function f1_old(u,v,t) {
+    let phi = Math.PI*1.8*v;
+    let theta = Math.PI*1.8*u;
+    const R0 = 3, R1 = 1;
+    let psi = phi*3 + theta*3 + t*3;
+    let r1 = R1 * (1+ 0.2*Math.sin(psi));
+    let r = R0 + r1*Math.cos(theta);
+    return new BABYLON.Vector3(
+        r*Math.cos(phi), 
+        r1*Math.sin(theta),
+        r*Math.sin(phi));
+}
+
+function f1(u,v,t) {
+    
+    let phi = 2 * Math.PI * u, csPhi = Math.cos(phi), snPhi = Math.sin(phi);
+    // v = v * m_maxV * 10;
+    let vv = v * 10;
+    let sech_v = 1/Math.cosh(vv);
+    let x = csPhi*sech_v;
+    let y = snPhi*sech_v ;
+    let z = vv - Math.tanh(vv) ;
+    let sc = 3;
+    return new BABYLON.Vector3(x*sc,-z*sc+3 - param1 * phi ,y*sc,);
+}
+
+function createGrid(scene) {
+    
+    let Color4 = BABYLON.Color4;
+    let Vector3 = BABYLON.Vector3;
+     
+    let m = 50;
+    let r = 5;
+    let pts = [];
+    let colors = [];
+    let c1 = new Color4(0.7,0.7,0.7,0.5);
+    let c2 = new Color4(0.5,0.5,0.5,0.25);
+    let cRed   = new Color4(0.8,0.1,0.1);
+    let cGreen = new Color4(0.1,0.8,0.1);
+    let cBlue  = new Color4(0.1,0.1,0.8);
+    
+    let color = c1;
+    function line(x0,y0,z0, x1,y1,z1) { 
+        pts.push([new Vector3(x0,y0,z0), new Vector3(x1,y1,z1)]); 
+        colors.push([color,color]); 
+    }
+    
+    for(let i=0;i<=m;i++) {
+        if(i*2==m) continue;
+        color = (i%5)==0 ? c1 : c2;
+        let x = -r+2*r*i/m;        
+        line(x,0,-r, x,0,r);
+        line(-r,0,x, r,0,x);
+    }
+    
+    let r1 = r + 1;
+    let a1 = 0.2;
+    let a2 = 0.5;
+    
+    // x axis
+    color = cRed;
+    line(-r1,0,0, r1,0,0); 
+    line(r1,0,0, r1-a2,0,a1);
+    line(r1,0,0, r1-a2,0,-a1);
+        
+    // z axis
+    color = cBlue;
+    line(0,0,-r1, 0,0,r1); 
+    line(0,0,r1, a1,0,r1-a2);
+    line(0,0,r1,-a1,0,r1-a2);
+    
+    // y axis
+    color = cGreen;
+    line(0,-r1,0, 0,r1,0); 
+    line(0,r1,0, a1,r1-a2,0);
+    line(0,r1,0,-a1,r1-a2,0);
+    line(0,r1,0, 0,r1-a2,a1);
+    line(0,r1,0, 0,r1-a2,-a1);
+    
+    const lines = BABYLON.MeshBuilder.CreateLineSystem(
+        "lines", {
+                lines: pts,
+                colors: colors,
+                
+        }, 
+        scene);
+    return lines;    
+};
+
+function populateScene(scene) {
+
+    createGrid(scene);
+
+    let srf = new Surface((u,v)=>f1(u,v,0), 70,70, scene);
+
+    let material = srf.mesh.material = new BABYLON.StandardMaterial('mat', scene);
+    material.twoSidedLighting = true;
+    material.backFaceCulling = false;
+    material.diffuseColor.set(0.9,0.9,0.9);
+    material.specularColor.set(0.3,0.3,0.3);
+
+    material.diffuseTexture = makeCheckboardTexture(scene);
+
+    let c1 = srf.mesh.createInstance('c1');
+    let c2 = srf.mesh.createInstance('c2');
+    
+    
+
+    // animazione
     scene.registerBeforeRender(() => {
-        let t = performance.now() * 0.001;
-        torus.rotation.x = t;
+
+        if(param1 == 0.0) {
+            c1.isVisible = c2.isVisible = false;
+        } else {
+            c1.isVisible = c2.isVisible = true;
+            c1.position.y = param1 * 2 * Math.PI;
+            c2.position.y = param1 * 2 * Math.PI * 2;
+
+        }
+        // tempo in secondi dopo l'inizio della visione della pagina
+        let seconds = performance.now() * 0.001;
+
+        srf.update((u,v) => f1(u,v,seconds));
+    
+
     });
 }
+
--- slides/pages/rolling-sphere/rolling-sphere.js
+++ slides/pages/rolling-sphere/rolling-sphere.js
@@ -57,18 +57,241 @@ function onResize() {
     engine.resize();
 }
 
-function populateScene() {
-    let torus = BABYLON.MeshBuilder.CreateTorus('torus',{
-        diameter:6,
-        thickness:1,
-        tessellation:70
+function createPlaneTexture() {
+    let sz = 1024;
+    var texture = new BABYLON.DynamicTexture(
+        'a', {width:sz, height:sz}, scene, true);
+    texture.wrapU = texture.wrapV = BABYLON.Texture.WRAP_ADDRESSMODE;
+    let ctx = texture.getContext();
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0,0,sz,sz);
+    let xs = [0,sz/6,sz/4,sz/3,sz/2,2*sz/3,3*sz/4,sz*5/6,sz];
+    let ys = [0,sz/4,sz/2,3*sz/4,sz];
+    ctx.fillStyle = 'black';
+    [
+        [4,2,2,1,3,0],
+        [4,2,4,0,5,0],
+        [4,2,6,1,7,2],
+        [4,2,6,3,5,4],
+        [4,2,4,4,3,4],
+        [4,2,2,3,1,2],
+        [0,0,2,1,1,2],
+        [5,0,8,0,6,1],
+        [8,0,8,2,7,2],
+        [7,2,8,4,6,3],
+        [0,4,2,3,3,4],
+        [0,4,0,2,1,2]
+        
+        
+    ].forEach(([x0,y0,x1,y1,x2,y2]) => {
+        ctx.beginPath();
+        ctx.moveTo(xs[x0],ys[y0]);
+        ctx.lineTo(xs[x1],ys[y1]);
+        ctx.lineTo(xs[x2],ys[y2]);
+        ctx.closePath();
+        ctx.fill();    
+    });
 
+    
+
+    texture.update();
+    
+    return texture;
+}
+
+let ground;
+let texture;
+
+function populateScene() {
+    let sphere = BABYLON.MeshBuilder.CreateSphere('sphere',{
+        diameter:6
     },scene);
-    torus.material = new BABYLON.StandardMaterial('mat',scene);
-    torus.material.diffuseColor.set(0.8,0.4,0.1);
+    sphere.position.set(0,0,0)
+
+
+    sphere.material = new BABYLON.ShaderMaterial("mat", scene, {
+        vertex: "prova",
+        fragment: "prova",
+    },
+    {
+        attributes: ["position", "normal"],
+        uniforms: [
+            "world", "worldView", 
+            "worldViewProjection", 
+            "view", "projection", 
+            "time"],
+        samplers: ["myTexture"]
+    });
+   // material.backFaceCulling = false;
+
+
+    ground = BABYLON.MeshBuilder.CreateGround('plane', {
+        width:50,
+        height:50
+    }, scene)
+    let qx = 20, qy = qx*Math.sqrt(3);
+    ground.setVerticesData(BABYLON.VertexBuffer.UVKind, [0,0,qx,0,0,qy,qx,qy])
+    ground.position.y = -3
+    ground.material = new BABYLON.StandardMaterial('a', scene);
+    ground.material.specularColor.set(0,0,0);
+
+    texture = ground.material.diffuseTexture = createPlaneTexture();
+
+
+    sphere.material.setTexture("myTexture", texture);
 
     scene.registerBeforeRender(() => {
         let t = performance.now() * 0.001;
-        torus.rotation.x = t;
+        // torus.rotation.x = t;
+        sphere.position.x = Math.sin(t);
     });
 }
+
+
+
+
+BABYLON.Effect.ShadersStore["provaVertexShader"]= `
+
+precision highp float;
+
+// Attributes
+attribute vec3 position;
+attribute vec3 normal;
+attribute vec2 uv;
+
+// Uniforms
+uniform mat4 worldViewProjection;
+
+// Varying
+varying vec4 vPosition;
+varying vec3 vNormal;
+
+void main() {
+
+    vec4 p = vec4( position, 1. );
+
+    vPosition = p;
+    vNormal = normal;
+
+    gl_Position = worldViewProjection * p;
+
+}
+`;
+
+BABYLON.Effect.ShadersStore["provaFragmentShader"]= `
+precision highp float;
+
+uniform mat4 worldView, world, view;
+
+varying vec4 vPosition;
+varying vec3 vNormal;
+
+uniform sampler2D myTexture;
+
+void main(void) {
+
+
+    
+
+    vec3 cameraPos = (inverse(view)*vec4(0.0,0.0,0.0,1.0)).xyz;
+    vec3 pos = ( world * vPosition ).xyz;
+    vec3 norm = normalize( world * vec4(vNormal, 0.0) ).xyz;
+    vec3 cameraDir = normalize(cameraPos - pos).xyz;
+
+
+    vec3 r = normalize(reflect(-cameraDir, norm));
+
+    if(r.y>0.0)
+        gl_FragColor = vec4(0.0,1.0,1.0,1.0);
+    else
+    {
+        float h = -3.0;
+
+        float s = -(pos.y-h)/r.y;
+        if(s>=0.0) {
+            vec3 p = vec3(pos.x + r.x*s, -1.0, pos.z + r.z*s);
+
+            vec4 color = texture2D(myTexture, 0.3*vec2(p.x,p.z*sqrt(3.0)));
+            gl_FragColor = color;
+    
+        } else {
+            gl_FragColor = vec4(0.0,0.0,0.0,1.0);
+
+        }
+
+    }
+    
+    /*
+    
+    
+
+    if(r.y>=0.0)
+        gl_FragColor = vec4(0.0,1.0,1.0,1.0);
+    else 
+    {
+        float s = -(pos.y+2.0)/r.y;
+        vec3 p = vec3(pos.x + r.x*s, -1.0, pos.z + r.z*s);
+        float u = p.x - floor(p.x);
+        float v = p.z - floor(p.z);
+        gl_FragColor = vec4(u,v,1.0,1.0);
+        
+    }
+    */
+
+    /*
+    if(r.y>=0.0) gl_FragColor = vec4(1.0,1.0,0.0,1.0);
+    else
+    {
+        float s = vPosition.y/r.y;
+        vec3 p = vec3(vPosition.x + r.x*s, 0.0, vPosition.z + r.z*s);
+        float u = p.x - floor(p.x);
+        float v = p.z - floor(p.z);
+        gl_FragColor = vec4(u,v,0.0,1.0);
+        gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+    }
+    if(r.y>0.0)
+        gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+    else
+        gl_FragColor = vec4(0.0,1.0,0.0,1.0);
+    */
+
+}
+`;
+
+BABYLON.Effect.ShadersStore["MirrorBallFragmentShader"]= `
+precision highp float;
+varying vec2 vUV;
+varying vec3 v_norm;
+varying vec3 v_pos;
+varying float err;
+varying vec3 v_color;
+varying vec3 v_surfaceToLight;
+varying vec3 v_surfaceToView;
+
+vec4 lit(float l ,float h, float m) {
+    return vec4(1.0,
+                abs(l),//max(l, 0.0),
+                (l > 0.0) ? pow(max(0.0, h), m) : 0.0,
+                1.0);
+}
+
+
+// uniform sampler2D textureSampler;
+void main(void) {
+    if(err > 0.0 || abs(v_pos.x) > 5.0 || abs(v_pos.y) > 2.0 || abs(v_pos.z) > 2.0) discard;
+    else 
+    {
+        vec3 norm = normalize(v_norm);
+        vec3 surfaceToLight = normalize(v_surfaceToLight);
+        vec3 surfaceToView = normalize(v_surfaceToView);
+        vec3 halfVector = normalize(surfaceToLight + surfaceToView);
+    
+        if(dot(surfaceToView, norm)<0.0) {  norm = -norm; }
+        float cs = dot(norm, surfaceToLight);
+        vec4 litR = lit(cs,dot(norm, halfVector), 120.0);
+
+        vec3 color = v_color * litR.y + vec3(1.0,1.0,1.0) * litR.z;
+        gl_FragColor = vec4(color,1.0); // texture2D(textureSampler, vUV);    
+    }
+}
+`
